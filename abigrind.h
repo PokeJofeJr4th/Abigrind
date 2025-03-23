8b3b2ecacdc36ed98a434a8bcb5ada5602611e65
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2025-03-23 15:40:24 -0400

    add realloc and strdup. Hopefully they work!

--- abigrind.h
+++ abigrind.h
@@ -3,14 +3,19 @@
 #include <stdlib.h>
 
 #ifdef ENABLE_ABIGRIND
-#define malloc_tagged(x, tag) malloc_abi(x, __FILE__, __LINE__, tag)
-#define malloc(x) malloc_abi(x, __FILE__, __LINE__, "<no tag>")
-#define free(x) free_abi(x, __FILE__, __LINE__)
-#define main main_abi
-void *malloc_abi(size_t size, char *file, int line, char *tag);
-void free_abi(void *ptr, char *file, int line);
+    #define malloc_tagged(x, tag) malloc_abigrind(x, __FILE__, __LINE__, tag)
+    #define malloc(x) malloc_abigrind(x, __FILE__, __LINE__, "<no tag>")
+    #define realloc_tagged(x, y, tag) realloc_abigrind(x, y, __FILE__, __LINE__, tag)
+    #define realloc(x, y) realloc_abigrind(x, y, __FILE__, __LINE__, "<no tag>")
+    #define strdup(x) strdup_abigrind(x, __FILE__, __LINE__)
+    #define free(x) free_abigrind(x, __FILE__, __LINE__)
+    #define main main_abi
+    void *malloc_abigrind(size_t size, char *file, int line, char *tag);
+    void *realloc_abigrind(char *ptr, size_t size, char *file, int line, char *tag);
+    char *strdup_abigrind(char *str, char *file, int line);
+    void free_abigrind(void *ptr, char *file, int line);
 #else
-#define malloc_tagged(x, tag) malloc(x)
+    #define malloc_tagged(x, tag) malloc(x)
 #endif
 
 #endif
--- main.c
+++ main.c
@@ -2,50 +2,56 @@
 #include <stdio.h>
 #include <string.h>
 
-int main_abi();
+int main_abigrind();
 
 struct AllocNode
 {
     struct AllocNode *next;
     struct AllocNode *previous;
     char *file;
+    char *tag;
     size_t num_bytes;
     int line_num;
-    char *tag;
 };
 
-static struct AllocNode *ABI_HEAD = NULL;
+static struct AllocNode *ABI_ALLOCATED = NULL;
 
-void *malloc_abi(size_t size, char *file, int line, char *tag)
+void *malloc_abigrind(size_t size, char *file, int line, char *tag)
 {
     struct AllocNode *ptr = malloc(sizeof(struct AllocNode) + size);
-    ptr->next = ABI_HEAD;
-    if (ABI_HEAD != NULL)
+    ptr->next = ABI_ALLOCATED;
+    if (ABI_ALLOCATED != NULL)
     {
-        ABI_HEAD->previous = ptr;
+        ABI_ALLOCATED->previous = ptr;
     }
     ptr->line_num = line;
     ptr->file = file;
     ptr->num_bytes = size;
     ptr->previous = NULL;
     ptr->tag = strdup(tag);
-    ABI_HEAD = ptr;
+    ABI_ALLOCATED = ptr;
     return ptr + 1;
 }
 
-void free_abi(void *ptr, char *file, int line)
+char *strdup_abigrind(char *str, char *file, int line)
+{
+    size_t len = strlen(str);
+    char *ptr = malloc_abigrind(len, file, line, "strdup");
+    strncpy(ptr, str, len);
+}
+
+void free_abigrind(void *ptr, char *file, int line)
 {
     struct AllocNode *alloc_ptr = ((struct AllocNode *)ptr) - 1;
 
-    struct AllocNode *cur = ABI_HEAD;
-
-    while (cur != NULL)
+    struct AllocNode *current = ABI_ALLOCATED;
+    while (current != NULL)
     {
-        if (cur == alloc_ptr)
+        if (current == alloc_ptr)
         {
             if (alloc_ptr->previous == NULL)
             {
-                ABI_HEAD = alloc_ptr->next;
+                ABI_ALLOCATED = alloc_ptr->next;
             }
             else
             {
@@ -59,16 +65,46 @@ void free_abi(void *ptr, char *file, int line)
             free(alloc_ptr);
             return;
         }
-        cur = cur->next;
+        current = current->next;
     }
     printf("Attempt to free unknown pointer %p at %s:%i.\n\t(this may be due to a double-free or the allocation coming from a file compiled without abigrind)\n", ptr, file, line);
     free(ptr);
 }
 
+void *realloc_abigrind(void *ptr, size_t size, char *file, int line)
+{
+    struct AllocNode *alloc_ptr = ((struct AllocNode *)ptr) - 1;
+
+    struct AllocNode *current = ABI_ALLOCATED;
+    while (current != NULL)
+    {
+        if (current == alloc_ptr)
+        {
+            alloc_ptr = realloc(alloc_ptr, sizeof(struct AllocNode) + size);
+            if (alloc_ptr->previous == NULL)
+            {
+                ABI_ALLOCATED = alloc_ptr;
+            }
+            else
+            {
+                alloc_ptr->previous->next = alloc_ptr;
+            }
+            if (alloc_ptr->next != NULL)
+            {
+                alloc_ptr->next->previous = alloc_ptr;
+            }
+            return (void *)(alloc_ptr + 1);
+        }
+        current = current->next;
+    }
+    printf("Attempt to realloc unknown pointer %p at %s:%i.\n\t(this may be due to a double-free or the allocation coming from a file compiled without abigrind)\n", ptr, file, line);
+    return realloc(ptr, size);
+}
+
 int main()
 {
-    int result = main_abi();
-    struct AllocNode *node = ABI_HEAD;
+    int result = main_abigrind();
+    struct AllocNode *node = ABI_ALLOCATED;
     while (node != NULL)
     {
         printf("%lli bytes at %s:%i not freed: %s\n", node->num_bytes, node->file, node->line_num, node->tag);
